f51180d991e86e68f26e0bcb05a7169ce583ed4a
Mahmoud Alaskndrany <ma7m0d_alx@aucegypt.edu>
2025-11-08 23:49:44 +0200

    prepare report and readme and prepare for submission

---
+++ build/CMakeFiles/QM_Algorithm_Implementation.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/expression.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/expression.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/file-parser.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/file-parser.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/implicant.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/implicant.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/main.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/main.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/qm-minimizer.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/qm-minimizer.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/quine-mccluskey-driver.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/quine-mccluskey-driver.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/verilog-generator.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/src/verilog-generator.cpp.obj.d"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/utils/verilog_utils.cpp.obj"
+  "CMakeFiles/QM_Algorithm_Implementation.dir/utils/verilog_utils.cpp.obj.d"
+  "QM_Algorithm_Implementation.exe"
+  "QM_Algorithm_Implementation.exe.manifest"
+  "QM_Algorithm_Implementation.pdb"
+  "libQM_Algorithm_Implementation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QM_Algorithm_Implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
